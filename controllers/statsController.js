830e2644468d968b53aaf355850db8de0ad2c4f8
yassermzh <yassermzh@gmail.com>
2025-05-16 23:42:15 +0200

    use event and broadcast to communicate between controllers

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,7 +1,16 @@
-angular.module("todoApp").controller("StatsController", function ($scope) {
-  $scope.totalTodos = 0;
-  $scope.completedTodos = 0;
+angular
+  .module("todoApp")
+  .controller("StatsController", function ($rootScope, $scope) {
+    $scope.totalTodos = 1;
+    $scope.completedTodos = 0;
 
-  // We'll need a way to update these stats when the todos in
-  // TodoController change. For now, we'll just initialize them.
-});
+    // We'll need a way to update these stats when the todos in
+    // TodoController change. For now, we'll just initialize them.
+    $rootScope.$on("todosUpdated", function (event, todos) {
+      console.log("todosUpdated");
+      $scope.totalTodos = todos.length;
+      $scope.completedTodos = todos.filter(function (todo) {
+        return todo.done;
+      }).length;
+    });
+  });
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,21 +1,24 @@
-angular.module("todoApp").controller("TodoController", function ($scope) {
-  $scope.todos = [
-    { text: "Learn AngularJS 1", done: true },
-    { text: "Build a todo app", done: false },
-  ];
+angular
+  .module("todoApp")
+  .controller("TodoController", function ($rootScope, $scope) {
+    $scope.todos = [
+      { text: "Learn AngularJS 1", done: true },
+      { text: "Build a todo app", done: false },
+    ];
 
-  $scope.newTodoText = "";
+    $scope.newTodoText = "";
 
-  $scope.addTodo = function () {
-    if ($scope.newTodoText) {
-      $scope.todos.push({ text: $scope.newTodoText, done: false });
-      $scope.newTodoText = "";
-      $scope.$broadcast("todosUpdated", $scope.todos); // Broadcast an event
-    }
-  };
+    $scope.addTodo = function () {
+      console.log("addTodo");
+      if ($scope.newTodoText) {
+        $scope.todos.push({ text: $scope.newTodoText, done: false });
+        $scope.newTodoText = "";
+        $rootScope.$broadcast("todosUpdated", $scope.todos); // Broadcast an event
+      }
+    };
 
-  $scope.removeTodo = function (index) {
-    $scope.todos.splice(index, 1);
-    $scope.$broadcast("todosUpdated", $scope.todos); // Broadcast an event
-  };
-});
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+      $rootScope.$broadcast("todosUpdated", $scope.todos); // Broadcast an event
+    };
+  });
